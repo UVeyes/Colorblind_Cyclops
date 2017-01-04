07a15c3d2faba2db20dcc7ee4f633d4ec0503c28
Val-Apana <valentin.siderskiy@apana.com>
2017-01-04 01:57:51 -0800

    Finding Dark Spots on UV photos
    Using UV and VL photos. Finding Dark UV spots.

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ flowersDir = imagesDir + '\Flowers'
 landscapeDir = imagesDir + '\Petroglyphs' 
 
 #change directory for landscape
-os.chdir(landscapeDir)
+os.chdir(flowersDir)
 
-RED_THRESHOLD = 100
-BLUE_GREEN_THRESHOLD = 50
+UV_THRESHOLD = 40
+BLUE_GREEN_RED_THRESHOLD = 100
 
-UVraw = rawpy.imread('petroglyphs_uvBaader_20160225valleyOfFireStateParkNV_45180.nef')	#Creates RAWPY Object
-VLraw = rawpy.imread('petroglyphs_visible_20160225valleyOfFireStateParkNV_45170.nef')  #Creates RAWPY Object
+UVraw = rawpy.imread('bouquet_uvAndreaU2_sun_20160914wf_50784.nef')	#Creates RAWPY Object
+VLraw = rawpy.imread('bouquet_visRef_sun_20160914wf_50701.nef')  #Creates RAWPY Object
 
 UVrgb = UVraw.postprocess()
 VLrgb = VLraw.postprocess()
@@ -43,79 +43,38 @@ VLred = VLbgr.copy()
 VLred[:,:,0] = 0
 VLred[:,:,1] = 0
 
-VLbS 		= cv2.resize(VLb, (960, 540))  
-cv2.imshow('VLbS',VLbS)
+VLblue_green_red = VLbgr.copy()
 
-VLbS 		= cv2.resize(VLb, (960, 540))  
-cv2.imshow('VLbS',VLbS)
+VLbgr_sum = VLr + VLb + VLg
 
-VLbS 		= cv2.resize(VLb, (960, 540))  
-cv2.imshow('VLbS',VLbS)
+ret, VLbgr_mask = cv2.threshold(VLbgr_sum,BLUE_GREEN_RED_THRESHOLD,512,cv2.THRESH_BINARY)
+ret, UV_mask = cv2.threshold(UVbw,UV_THRESHOLD,255,cv2.THRESH_BINARY)
 
+UV_mask_inv = cv2.bitwise_not(UV_mask)
+VLbgr_mask_inv = cv2.bitwise_not(VLbgr_mask)
 
+UV_only = cv2.bitwise_and(VLbgr_mask,UV_mask_inv) #no blue or green or black
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+VLblue_green_red_UV_AR = VLblue_green_red.copy()
+VLblue_green_red_UV_AR[:,:,0] += UV_only*50
+VLblue_green_red_UV_AR[:,:,2] += UV_only*50
+#cv2.imshow('red_blue_green_UV_AR',VLred_blue_green_UV_AR)
+VLbgr_AR_UV_S 	= cv2.resize(VLblue_green_red_UV_AR, (960, 540))  
+UV_onlyS     = cv2.resize(UV_only, (960, 540))
+UV_maskS 	= cv2.resize(UV_mask, (960, 540))
+VLbgr_maskS 	= cv2.resize(VLbgr_mask, (960, 540))
 
-VLblue_green = VLbgr.copy()
-VLblue_green[:,:,2] = 0
-
-VLred_threshold_image = VLbgr.copy()
-VLred_threshold_image[:,:,0] = 0
-VLred_threshold_image[:,:,1] = 0
-VLred_threshold_image[:,:,2] = RED_THRESHOLD
-
-VLbg = VLb + VLg
-
-ret, VLbg_mask = cv2.threshold(VLbg,BLUE_GREEN_THRESHOLD,512,cv2.THRESH_BINARY)
-ret, VLr_mask = cv2.threshold(VLr,RED_THRESHOLD,255,cv2.THRESH_BINARY)
-VLbg_mask_inv = cv2.bitwise_not(VLbg_mask)
-
-VLred_only = cv2.bitwise_and(VLbg_mask_inv,VLr_mask) #no blue or green or black
-
-VLblue_green_r_AR = VLblue_green.copy()
-VLblue_green_r_AR[:,:,1] += VLred_only*20
-#cv2.imshow('blue_green_r_AR',VLblue_green_r_AR)
-#cv2.imshow('bg_mask_inv',VLbg_mask_inv)
-#cv2.imshow('red_only', VLred_only)
-#cv2.imshow('bg_mask', VLbg_mask)
-#cv2.imshow('r_mask', VLr_mask)
-#cv2.imshow('RED_THRESHOLD',VLred_threshold_image)
-#cv2.imshow('red_only', red_only)
-#cv2.imshow('bg',bg)
-#cv2.imshow('r',r)
-
-#cv2.imshow('image',VLbgr)
-#cv2.imshow('blue',blue)
-#cv2.imshow('green',green)
-#cv2.imshow('red',VLred)
-#cv2.imshow('blue_green',VLblue_green)
-
+cv2.imshow('VLbgr_AR_UV_S',VLbgr_AR_UV_S)
+cv2.imshow('UV_onlyS',UV_onlyS)
+cv2.imshow('UV_maskS',UV_maskS)
+cv2.imshow('VLbgr_maskS',VLbgr_maskS)
 
 #small (resized images)
 VLbgrS 	= cv2.resize(VLbgr, (960, 540))  
 UVbgrS 	= cv2.resize(UVbgr, (960, 540))  
 UVbwS 	= cv2.resize(UVbw, (960, 540))  
 
-#VLrS 		= cv2.resize(VLr, (960, 540))  
-#VLbS 		= cv2.resize(VLg, (960, 540))  
-#VLgS 		= cv2.resize(VLb, (960, 540))  
-
-UVrS 		= cv2.resize(UVr, (960, 540))  
-UVbS 		= cv2.resize(UVg, (960, 540))  
-UVgS 		= cv2.resize(UVb, (960, 540))  
-
-cv2.imshow('VLbgrS',VLbgrS)
-cv2.imshow('UVbgrS',UVbgrS)
 cv2.imshow('UVbwS',UVbwS)
-
-#cv2.imshow('VLrS', VLrS)
-#cv2.imshow('VLbS', VLbS)
-#cv2.imshow('VLgS', VLgS)
-cv2.imshow('UVrS', UVrS)
-cv2.imshow('UVbS', UVbS)
-cv2.imshow('UVgS', UVgS)
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
